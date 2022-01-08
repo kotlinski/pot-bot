1d34dce8dbcd7f6b2c71046feb77eb62e86ea354
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-01-08 23:27:33 +0100

    chore(clean-up): remove mocha

--- .mocharc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'ui': 'bdd',
-  'check-leaks': true,
-  'recursive': true,
-  'slow': 200,
-  'reporter': 'spec',
-  'require': ['babel-core/register', '@springworks/test-harness'],
-};
