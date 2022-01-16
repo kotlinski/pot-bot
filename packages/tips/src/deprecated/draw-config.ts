17aa60e0514cab6ab385c06f56f53549f6ada84b
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-01-16 20:23:38 +0100

    chore(deprecation): remove deprecated files

--- packages/tips/src/deprecated/draw-config.ts
+++
@@ -1,7 +0,0 @@
-export interface DrawConfigV1 {
-  number_of_focused_bets_to_align: number;
-  game_type: 'stryktipset' | 'europatipset';
-  number_of_lines_to_pick: number;
-  bet_value_quota: number;
-  number_of_X_signs: number;
-}
