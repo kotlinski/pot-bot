90ad4ca66d042b9c303c45c3b1109c45b9ec5594
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-01-17 23:40:27 +0100

    chore(lambda): update console logs

--- packages/lambda/src/index.ts
+++ packages/lambda/src/index.ts
@@ -1,5 +1,5 @@
 export function handler(): void {
   console.log('hello world!');
 
-  console.log('this change should make things look edited');
+  console.log('this change should make things look edited!!!');
 }
