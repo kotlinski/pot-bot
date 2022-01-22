8f508d4f71cde262742be4b6f7f9174f1332018b
Simon Kotlinski <simon.kotlinski@gmail.com>
2022-01-22 20:00:13 +0100

    feature(lambda): download and store draw (#25)

---
+++ packages/tips/src/index.ts
@@ -0,0 +1,7 @@
+import { GameType } from './script/command-line-interfaces';
+import DropboxStore from './storage/dropbox-store/dropbox-store';
+import SvenskaSpelApiClient from './svenska-spel/api-clients/svenska-spel-api-client';
+import DrawProvider from './svenska-spel/draw/draw-provider';
+
+export { DrawProvider, SvenskaSpelApiClient, DropboxStore };
+export { GameType };
